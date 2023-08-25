5edced80f1551eeaadbcf697a92302565fc1c443
Md Riyad Hossain <140542958+riyadnill9@users.noreply.github.com>
2023-08-25 09:17:14 +0600

    animated-headline a perfect animation headline

---
+++ main.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(function(){$('.header').animatedHeadline({animationType:'slide'})})})
+$(document).ready(function(){$(".btn").click(function(){$(".nav_ul").fadeToggle(`slow`)})})
